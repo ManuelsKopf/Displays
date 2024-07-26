e050e2ed480e3306946aab265d8c44f75441c1b2
fraenklj <johannes.fraenkle@web.de>
2024-07-26 20:35:52 +0200

    RGB neu

--- Source/rgb.py
+++ Source/rgb.py
@@ -3,14 +3,41 @@ import cv2
 from PIL import Image, ImageOps
 import numpy as np
 import matplotlib.pyplot as plt
-
-input_image_path = "Base pictures\Image1.jpeg"
-# Laden und Konvertieren des Bildes
-rgb_image = Image.open(input_image_path)
-cmyk_image = rgb_image.convert('RGB')
-
-# Speichern des konvertierten Bildes unter einem neuen Pfad
-output_image_path_2 = 'Generated Pictures\converted_image_rgb.jpg'
-cmyk_image.save(output_image_path_2)
+import os
 
 
+input_folders = [1, 2, 4, 7, 11, 12, 13, 14, 15, 17, 18, 21, 22, 23, 26, 27, 32, 33, 35, 38]
+
+base_input_dir = 'C:/Users/johan/VSC-Projects/Displays/Quadratisch Praktisch Guut'
+
+base_output_dir = 'C:/Users/johan/VSC-Projects/Displays/Generated Pictures'  
+
+
+def bearbeiten(input_image_path, output_image_path):
+    with Image.open(input_image_path) as img:   
+        img = img.convert('RGB')
+        pixels = img.load()
+
+        for i in range(img.width):
+            for j in range(img.height):
+                r, g, b = pixels[i, j]
+                pixels[i, j] = (255 - r, 255 - g, 255 - b)
+    
+            img.save(output_image_path)
+
+for folder_number in input_folders:
+    input_folder = os.path.join(base_input_dir, str(folder_number))
+    output_folder = os.path.join(base_output_dir, str(folder_number))
+    os.makedirs(output_folder, exist_ok=True)  
+
+    # Alle Dateien im Eingabeordner durchlaufen
+    for filename in os.listdir(input_folder):
+        if filename.startswith(('IMG')):
+            input_image_path = os.path.join(input_folder, filename)
+            output_image_path = os.path.join(output_folder, filename)
+            
+            # Bild bearbeiten und speichern
+            bearbeiten(input_image_path, output_image_path)
+
+
+print('Alle Bilder wurden bearbeitet.')
